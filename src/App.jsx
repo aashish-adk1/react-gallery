2ca4dd54fbd014460affea384839c3b3ea4dee3d
Aashish Adhikari <aashish.adk156@gmail.com>
2025-02-05 13:48:54 +0545

    Hero section styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
-
+import './index.css'
+import bgImg from './assets/Bg.jpg'
 function App() {
   return (
     <>
+    <section className='heroSection'>
+    <div className="wrapper">
       <Navbar/>
       <Hero/>
+      </div>
+      </section>
     </>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import bgImage from '../../assets/Bg.jpg'
-import './Hero.css'
+import profileImg from '../../assets/profile-pic.jpg'
 function Hero() {
   return (
-    <div className="Container">
+    <div className="container">
       <div className="intro">
         <h1 className="title">Hi I'm Aashish Adhikari.</h1>
         <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum corporis quisquam ad porro doloremque qui facilis, cum, ratione tenetur perspiciatis culpa, quasi odio vitae delectus.</p>
+        <button className="learnmore">Learn More</button>
+
       </div>
       <div className="profile-pic">
-        <img src={bgImage} alt="background image" />
+        <img src={profileImg} alt="Profile Picture" />
       </div>
     </div>
   )
